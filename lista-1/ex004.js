dee9834e6137d1d409c15b17cc02e1075befbc7e
Maria Eduarda dos Santos Lara <eduardalara5921@gmail.com>
2025-09-02 11:31:07 -0300

    ex004.js

---
+++ lista-1/ex004.js
@@ -0,0 +1,10 @@
+import input from 'readline-sync'
+
+let n1 = input.questionInt("Digite o primeiro numero: ")
+let n2 = input.questionInt("Digite o segundo numero: ")
+
+n1 = (n1 * 2);
+n2 = (n2 / 2);
+
+console.log("Resposta 1:", n1);
+console.log("Resposta 2:", n2);
