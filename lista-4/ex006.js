e0bd6630d814e1e18e80786d67f2731e6e16f806
Maria Eduarda dos Santos Lara <eduardalara5921@gmail.com>
2025-09-11 12:07:29 -0300

    ex006.js

---
+++ lista-4/ex006.js
@@ -0,0 +1,10 @@
+let input = require('readline-sync')
+
+let n1 = input.questionInt("Digite um numero: ");
+let n2 = input.questionInt("Digite um segundo numero: ");
+
+if(n1<n2) {
+    console.log( n1, n2);
+} else {
+    console.log( n2, n1);
+}
